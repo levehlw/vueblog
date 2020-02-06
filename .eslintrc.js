63db23d07066df6722c41032f3f3851af52b15da
Leve <836220632@qq.com>
2020-02-06 14:39:20 +0800

    主页

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,
+    'no-unused-expressions': 0,
+    'no-labels': 0,
+    'indent': 0,
+    'vue/no-unused-vars': 0,
+    'padded-blocks': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
